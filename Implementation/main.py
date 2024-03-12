08ae980f7a7c92a8209c32d94029c93f22b427d8
NisargMukeshbhaiPatel <nishupatel2550@icloud.com>
2024-03-12 18:55:40 +0530

    Add feature of drawing images in terminal

--- Implementation/main.py
+++ Implementation/main.py
@@ -6,21 +6,14 @@ from utils import ansi, ui
 def main(stdscr):
     curses.curs_set(0)
     ansi.set_init_pair_ansi_index()
-
-    w = stdscr.getmaxyx()[1] - 20
-    h = stdscr.getmaxyx()[0] - 20
-    pad = 10
-    win = curses.newwin(h + pad, w + pad, 5, 5)
-    gapx, gapy = ui.draw_board(6, 7, w, h, win)
-
-    ui.draw_rect_inside(0, 1, gapx, gapy, win)
-
-    ui.draw_rect_around(4, gapx, gapy * 6, win, 2)
-    time.sleep(.5)
-    ui.draw_rect_around(0, gapx, gapy * 4, win, 4)
-    stdscr.refresh()
-    win.refresh()
-    win.getch()
+    frames = 8
+    for i in range(1, 15):
+        i %= frames
+        stdscr.clear()
+        width = stdscr.getmaxyx()[1]/2 - 1
+        ansi.add_img_with_ansi(f"assets/CF/CF-{i}.png", width, stdscr)
+        stdscr.refresh()
+        time.sleep(.5)
 
 
 # if curses.has_colors():
--- Implementation/utils/ansi.py
+++ Implementation/utils/ansi.py
@@ -14,10 +14,32 @@ ANSI_COLORS = (
 )
 
 def set_init_pair_ansi_index():
-	curses.use_default_colors()
+    curses.use_default_colors()
 	# Set pairs according to ANSI_COLORS idx
-	# For 0, it's set to default background color of terminal and also curses doesn't allow setting it
-	for i in range(1, 8):
-		curses.init_pair(i, -1, ANSI_COLORS[i])
+    # For 0, it's set to default background color of terminal and also curses doesn't allow setting it
+    for i in range(1, 8):
+        curses.init_pair(i, -1, ANSI_COLORS[i])
 
 
+def add_img_with_ansi(path, cols, stdscr):
+    flg = False
+    my_art = AsciiArt.from_image(path)
+    ascii_output = my_art.to_ascii(columns=cols, char=" ")
+
+    lines = ascii_output.split("\n")
+	# print("LEN->", len(lines), " AkTUAL HEIGHT->", stdscr.getmaxyx()[0])
+
+    for line in lines:
+        parts = line.split("\x1b[")[1:]
+        for part in parts:
+        	idx = int(part.split("m")[0]) - 30
+        	# initialize color.set_init_pair_ansi_index() to access ansi indexed color-pairs
+        	stdscr.addch(" ", curses.color_pair(idx))	
+
+        try:
+        	stdscr.addch('\n')
+        except:
+            flg = True
+            break
+    if flg:
+        warnings.warn(f"{path} Image can't be render fully in avail space")
