833ebe1a5ae42bfb7d5f25bb13308a6aac411829
NisargMukeshbhaiPatel <nishupatel2550@icloud.com>
2024-03-12 17:33:08 +0530

    Add warnings for insufficient window space while drawing

--- Implementation/main.py
+++ Implementation/main.py
@@ -24,6 +24,7 @@ def set_init_pair_ansi_index():
 
 # TODO handle x overflow
 def draw_board(rows, cols, width, height, win):
+    flag = False
     gapx = width // cols
     gapy = height // rows + 1
 
@@ -41,23 +42,31 @@ def draw_board(rows, cols, width, height, win):
             try:
                 win.addch(i, j * gapx, " ", curses.color_pair(4))
             except:
+                flag = True
                 break
+    if flag:
+        warnings.warn("Attempted to draw game board in insufficient win space")
 
     return [gapx, gapy]
 
 def draw_rect_inside(row, col, w, h, win):
     x = col + 1 if col == 0 else col * w + 1
     y = row + 1 if row == 0 else row * h + 1
+    flag = False
 
     for i in range(y, y + h - 1):
         for j in range(x, x + w - 1):
             try:
                 win.addch(i, j, " ", curses.color_pair(3))
             except:
+                flag = True
                 break
+    if flag:
+        warnings.warn("Attempted to draw_rect_inside insufficient win space")
 
 
 def draw_rect_around(col, w, h, win, color_pair_idx):
+    flag = False
     x = col * w
     y = 0
     # i -> rows, j -> cols
@@ -69,7 +78,10 @@ def draw_rect_around(col, w, h, win, color_pair_idx):
                 try:
                     win.addch(i, j, " ", curses.color_pair(color_pair_idx))
                 except:
+                    flag = True
                     break
+    if flag:
+        warnings.warn("Attempted to draw_rect_around insufficient space")
 
 
 def main(stdscr):
@@ -81,14 +93,16 @@ def main(stdscr):
     pad = 10
     win = curses.newwin(h + pad, w + pad, 5, 5)
     gapx, gapy = draw_board(6, 7, w, h, win)
+    win.refresh()
+    win.getch()
 
     draw_rect_inside(0, 1, gapx, gapy, win)
 
     draw_rect_around(4, gapx, gapy * 6, win, 2)
     time.sleep(.5)
     draw_rect_around(0, gapx, gapy * 4, win, 4)
+    stdscr.refresh()
     win.refresh()
-    win.getch()
 
 
 # if curses.has_colors():
